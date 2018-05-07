821710f41846cd7b6a816cee751caed9420cf29a
Huangyep <lena0409@qq.com>
2018-05-07 14:54:06 +0800

    新增新建主题、添加内容、修改内容的功能

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -13,5 +13,14 @@ urlpatterns = [
     # 显示主题的详细内容
     re_path('topics/(?P<topic_id>\d+)/' , views.topic , name='topic'),
 
+    # 用于添加新主题的网页
+    path('new_topic/' , views.new_topic , name='new_topic'),
+
+    # 用于添加新条目的页面
+    re_path('new_entry/(?P<topic_id>\d+)/',views.new_entry , name='new_entry'),
+
+    # 用于编辑已有条目的页面
+    re_path('edit_entry/(?P<entry_id>\d+)/',views.edit_entry , name='edit_entry'),
+
 ]
 app_name = 'learning_logs'
